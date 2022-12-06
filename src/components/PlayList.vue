dbcefc74335130c49ef77c5d6191b4c872e04405
Tylerdays <tylerdday.software@gmail.com>
2022-12-06 03:32:08 -0700

    added a new component, started building methods to add and remove songs from playlists

---
+++ src/components/PlayList.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <ul>
+            <li v-for="song in songs" :key="song.id">
+            {{song.title}} - {{song.artist}}</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'PlayList',
+        data () {
+            return {
+                songs: []
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/SongList.vue
@@ -0,0 +1,57 @@
+<template>
+    <div>
+        <ul>
+            <li v-for="song in songs" :key="song.id">
+                {{song.title}} - {{song.artist}}
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'SongList',
+        data () {
+            return{
+                songs: [
+                    {
+                        id: 1,
+                        title: 'Temp title1',
+                        artist: 'artist1',
+                    },
+                    {
+                        id: 2, 
+                        title: 'Temp title2',
+                        artist: 'artist2',
+                    },
+                    {
+                        id: 3,
+                        title: 'Temp title3',
+                        artist: 'artist3',
+                    },
+                    {
+                        id: 4,
+                        title: 'Temp title4',
+                        artist: 'artist4',
+                    },
+                    {
+                        id: 5,
+                        title: 'Temp title5',
+                        artist: 'artist5',
+                    },
+                ]
+            }
+        },
+        methods: {
+            //first we have to remove the song from the songlist
+            addSongToPlaylist(song) {
+                const index = this.songs.findIndex(s => s.id === song.id);
+                this.songs.splice(index, 1);
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
