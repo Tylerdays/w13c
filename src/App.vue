e0fc10ab40b2d8d141095bb4fcb3f26392b7a36b
Tylerdays <tylerdday.software@gmail.com>
2022-12-06 03:23:03 -0700

    fixed an issue where SongList was propigated as Scss instead of css

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <SongList/>
   </div>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import SongList from './components/SongList.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    SongList,
   }
 }
 </script>
